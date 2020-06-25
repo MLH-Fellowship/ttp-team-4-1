da4ac4aeba74c3d8e6b0eda015c9e9375331f4fc
unknown <klam9986@bths.edu>
2020-06-25 00:31:13 -0400

    modified index.js to include a emit for chatmessage in the backend

--- server/index.js
+++ server/index.js
@@ -3,14 +3,35 @@ const Game = require("./game");
 
 const game = new Game(server.io);
 
+let interval;
+
 server.io.on("connection", (socket) =>
 {
+    console.log("New client connected");
+
+    if(interval) clearInterval(interval);
+    interval = setInterval(() => getSocketAndEmit(socket), 1000);
+
     game.connect(socket);
+
+    socket.on('chat message', (chatMsg) => {
+        io.emit('chat message', chatMsg);
+    });
 
     socket.on("disconnect", () =>
     {
+        console.log("Client disconnected");
+        clearInterval(interval);
         game.disconnect(socket);
     });
 });
+
+const getSocketAndEmit = (socket) =>
+{
+    const response = socket.id;
+
+    // Emitting a new message, will be received by client
+    socket.emit("GetSocketID", response);
+};
 
 game.start();
